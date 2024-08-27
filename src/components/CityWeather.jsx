a7d450b71512e2415b4afc7f485d1d3af32aaeac
Tayyab <m.tayyab33600@gmail.com>
2024-08-28 01:24:47 +0500

    fetch response from api and display

--- src/components/CityWeather.jsx
+++ src/components/CityWeather.jsx
@@ -1,13 +1,45 @@
 import React, { useEffect, useState } from "react";
 
 function CityWeather({ city }) {
-  const [weather, setWeather] = useState("");
+  const [weatherData, setWeatherData] = useState(null);
+  const [error, setError] = useState(null);
+
   useEffect(() => {
-    setWeather(`Here data will be populated for the ${city}`);
-  });
+    async function fetchWeatherData() {
+      try {
+        const res = await fetch(
+          `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=8836c24544aebfcbb61ba4b064f23d9e&units=metric`
+        );
+        if (!res.ok) {
+          throw new Error("Response was not ok!");
+        }
+        const data = await res.json();
+        setWeatherData(data);
+        setError(null);
+      } catch (error) {
+        console.error("Fetch error: ", error);
+        setError(
+          "Failed to fetch weather data. Please check the city's spelling and try again."
+        );
+        setWeatherData(null);
+      }
+    }
+
+    fetchWeatherData();
+  }, [city]); 
+
   return (
     <div>
-      <div>{weather}</div>
+      {error && <p>{error}</p>}
+      {weatherData ? (
+        <div>
+          <h2>{weatherData.name}</h2>
+          <p>Temperature: {weatherData.main.temp}°C</p>
+          <p>Weather: {weatherData.weather[0].description}</p>
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
     </div>
   );
 }
